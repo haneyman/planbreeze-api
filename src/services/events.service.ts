f57c3e08cdc1310290a2b97b1aeec3a91da2b48c
Mark Haney <mark@markhaney.net>
2022-06-19 18:27:54 -0700

    insert wip

--- src/services/events.service.ts
+++ src/services/events.service.ts
@@ -6,17 +6,26 @@ export class EventsService {
   constructor(@Inject(PG_CONNECTION) private conn: any) {}
   // private readonly events: Event[] = [];
 
-  // create(event: Event) {
-  // this.events.push(event);
-  // }
-
-  // findAll(): Event[] {
-  // return this.events;
-  // }
-
   async getEvents() {
     const res = await this.conn.query('SELECT * FROM events');
     console.log('res.rows: ', res.rows);
     return res.rows;
+  }
+
+  async create(event: Event) {
+    // const res = await this.conn.query('INSERT INTO events');
+    // console.log('res.rows: ', res.rows);
+    // return res.rows;
+    const text = 'INSERT INTO events() VALUES($1, $2) RETURNING *';
+    const values = ['brianc', 'brian.m.carlson@gmail.com'];
+
+    // async/await
+    try {
+      const res = await client.query(text, values);
+      console.log(res.rows[0]);
+      // { name: 'brianc', email: 'brian.m.carlson@gmail.com' }
+    } catch (err) {
+      console.log(err.stack);
+    }
   }
 }
